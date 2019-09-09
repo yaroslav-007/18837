756b14201e63c79f856b5438094e992ab58c5323
Yaroslav <yorimark@abv.bg>
2019-09-09 08:46:33 +0300

    upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "us-east-1"
+}
+
+
+resource "aws_db_instance" "56-xxxx" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  final_snapshot_identifier = "foo"
+}
